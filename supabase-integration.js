bd7406facc42e1678f35689b62e28459933ce70a
abubakrkhlfallh <braahkhlfallh@gmail.com>
2025-12-25 07:40:06 +0200

    Update supabase-integration.js

--- supabase-integration.js
+++ supabase-integration.js
@@ -1,5 +1,11 @@
 // supabase-integration.js - ربط مع قاعدة البيانات
+const SUPABASE_URL = "https://yqtaszzlibfocaonrygf.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxdGFzenpsaWJmb2Nhb25yeWdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0NjkzNTAsImV4cCI6MjA4MjA0NTM1MH0.mRu7m-xeW162j13seV1v7J_mdKWpKLEMsfkjffrAhnw";
 
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 // تهيئة عميل Supabase
 function initSupabase() {
     // تحقق من توفر مكتبة supabase
